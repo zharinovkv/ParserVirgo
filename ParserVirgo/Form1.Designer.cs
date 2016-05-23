a2b1c4ae6baecab0b3caa707286e57296ae5ea98
zharinovkv <zharinovkv@gmail.com>
2016-05-23 15:59:37 +0500

    Первый коммит.

---
+++ ParserVirgo/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ParserVirgo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Thread = new System.ComponentModel.BackgroundWorker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Thread
+            // 
+            this.Thread.WorkerReportsProgress = true;
+            this.Thread.WorkerSupportsCancellation = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(651, 190);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(669, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 221);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(651, 190);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(669, 221);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Старт";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 458);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "ParserVirgo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker Thread;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ParserVirgo/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using xNet;
+using ParserVirgo;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using ParserVirgo.Proxi;
+using System.Threading.Tasks;
+
+namespace ParserVirgo
+{
+    public partial class Form1 : Form
+    {
+        List<string> urllist = new List<string>();
+        SearcherAvito ks;
+        //SearcherGdeperevesti gps;
+        //List<ProductList> pl = new List<ProductList>();
+        List<SearcherAvito> productList = new List<SearcherAvito>();
+        
+        string time = DateTime.Now.ToString("_yyyy.MM.d_HH.mm");
+        List<string> goodProxiesList = new List<string>();
+        List<string> url = new List<string>();
+        List<string> list2 = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            ExecuteProxies();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        { }
+
+
+
+        // 21.00
+        // получаем список урлов
+        private async Task<List<string>> Work()
+        {
+            List<string> list = new List<string>(); 
+            int countPages = 1;
+            //Spyder spyder = new Spyder();
+
+            try
+            {
+                //получаем кол-во страниц                
+                //countPages = spyder.GetCountPages();
+            }
+
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+
+            try
+            {
+                for (int i = 1; i <= countPages; i++)
+                {
+                    // при присваивании переменной значения срабатывает событие в основном потоке
+                    list.AddRange(Spyder.GetListings(i));
+                }
+            }
+
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+
+            foreach(var item in list)
+            {
+                textBox1.Text += item + Environment.NewLine;
+            }
+
+            MessageBox.Show("Листинг готов.");
+            return list;
+        }
+
+
+        //private void ProgressPerevod(List<string> listUrls)
+        //{            
+        //    string path = @"C:\Users\User\Documents\ParserVirgo\Reports\textfile_";
+        //    System.IO.StreamWriter textFile = new System.IO.StreamWriter(path + time + ".txt");
+
+        //    for (int i = 0; i < listUrls.Count; i++)
+        //    {
+        //        gps = new SearcherGdeperevesti();
+        //        string sStr = "";
+
+        //        if (Convert.ToBoolean(gps.DownLoadHtml(listUrls[i], goodProxiesList)))
+        //        {
+        //            //gps.Url = listUrls[i];
+        //            //gps.FindTitle();
+        //            //gps.FindAdress();
+        //            //gps.FindEmail();
+        //            //gps.FindFax();
+        //            //gps.FindPhone();
+        //            //gps.FindSite();
+        //            //sStr = gps.FindString();
+        //            gps.FindString();
+        //        }
+
+        //        textFile.WriteLine(gps.Str);
+        //        //                textFile.WriteLine(sStr);
+        //        //textFile.WriteLineAsync(gps.Title + ";" + gps.Adress + ";" + gps.Email + ";" + gps.Fax + ";" + gps.Phone + ";" + gps.Site + ";");
+        //        //textFile.WriteAsync(gps.Title + ";" + gps.Adress + ";" + gps.Email + ";" + gps.Fax + ";" + gps.Phone + ";" + gps.Site + ";");
+        //        //textFile.WriteLine(gps.Title + ";" + gps.Adress + ";" + gps.Email + ";" + gps.Fax + ";" + gps.Phone + ";" + gps.Site + ";");
+        //        //textBox2.Text += sStr + Environment.NewLine;
+        //        textBox2.Text += gps.Str + Environment.NewLine;
+
+        //    }
+        //    textFile.Close();
+        //    MessageBox.Show("Парсинг готов.");
+        //}
+
+
+
+        // получаем спарсеный результат
+        private void Progress(List<string> listUrls)
+        {
+            //string path = @"C:\Users\User\Documents\ParserVirgo\Reports\textfile_";
+            //System.IO.StreamWriter textFile = new System.IO.StreamWriter(path + time + ".txt");
+
+            for (int i = 0; i < listUrls.Count; i++)
+            {
+                ks = new SearcherAvito();
+
+                if (Convert.ToBoolean(ks.DownLoadHtml(listUrls[i], goodProxiesList)))
+                {
+                    //ks.Url = listUrls[i];
+                    ks.FindUrl(listUrls[i]);
+                    ks.FindTitle();
+                    ks.FindCover();
+                }
+
+                string imageName = "";
+
+                if (ks.Url != null)
+                {
+                    string[] totoProcess = ks.Url.Split(new string[] { "/" }, StringSplitOptions.RemoveEmptyEntries);
+                    imageName = totoProcess[totoProcess.Length - 1] + ".png";
+                }
+
+                if (ks.Cover != null)
+                {
+                    SaveAs saveImage = new SaveAs();
+                    saveImage.SaveImage(ks, imageName, time);
+                    ks.ImagePath = imageName;
+                    productList.Add(ks);
+                    //textFile.WriteLine("a" + i + ". " + ks.Url + ";" + ks.Title + ";" + ks.Title + ".png" + ";" + Environment.NewLine);
+                    textBox2.Text += ks.Url + "\t" + ks.Title + "\t" + imageName + Environment.NewLine;
+                }
+                else
+                {
+                    ks.ImagePath = "NO IMAGE";
+                    productList.Add(ks);
+                    //textFile.WriteLine("b" + i + ". " + ks.Url + ";" + ks.Title + ";" + "No Image" + ";" + Environment.NewLine);
+                    textBox2.Text += ks.Url + "\t" + ks.Title + "\t" + "No Image" + Environment.NewLine;
+                }
+            }
+
+            SaveAs saveAs = new SaveAs();
+            //            textFile.Close();
+            saveAs.SaveAsCSV(productList, time);
+            MessageBox.Show("Парсинг готов.");
+        }
+
+
+        private async void ExecuteProxies()
+        {
+            PingProxi pingProxi = new PingProxi();
+            List<string> goods = await pingProxi.Example();
+            goodProxiesList = goods;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            list2 = await Work();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Progress(list2);
+            //ProgressPerevod(list2);
+        }
+    }
+}
